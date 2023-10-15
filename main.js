af2b6279aa37839a6eb479741056841953fd2785
Alexandra Dascalu <alexandradascalu145@gmail.com>
2023-10-15 17:31:31 +0300

    fetch

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML += `
-  <div>
-   <h2>Alexa1</h2>
-  </div>
-`;
+window.addEventListener("load", showProducts);
+
+async function showProducts() {
+  console.log("page fully loaded");
+  var response = await fetch(
+    "https://652bdb79d0d1df5273eeced1.mockapi.io/products"
+  );
+  var products = await response.json();
+
+  var productsDiv = document.getElementById("products");
+  var productsHTML = "";
+
+  for (var i = 0; i <= products.length - 1; i++) {
+    productsHTML += `<div class = "card">
+    <img src=${products[i].imageURL} />
+    </div>`;
+  }
+  productsDiv.innerHTML = productsHTML;
+}
